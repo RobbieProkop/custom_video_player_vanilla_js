58f03e9efc33ce1d10d76f979b205d963cec4efe
Robbie Prokop <robbieprokop@gmail.com>
2022-11-08 20:34:42 +0400

    begin js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // app
+//dom elements
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+//functions
+
+//event listeners
