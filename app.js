7e5f5e333ec62d15694867fd3340635f450197ae
Robbie Prokop <robbieprokop@gmail.com>
2022-11-08 20:45:06 +0400

    create base funcitons and event listeners

--- app.js
+++ app.js
@@ -8,4 +8,29 @@ const timestamp = document.getElementById("timestamp");
 
 //functions
 
+//used to play and pause the video
+const toggleVideoStatus = () => {};
+
+//update play/pause icon
+const updatePlayIcon = () => {};
+
+//update the progress and timestamp
+const updateProgress = () => {};
+
+// set video time to progress
+const setVideoProgress = () => {};
+
+//stops the video
+const stopVideo = () => {};
+
 //event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
